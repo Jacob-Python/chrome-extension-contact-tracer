7b2cbd298a26d0e4561306940f95716edf1b9b62
Jacob-Python <58337545+Jacob-Python@users.noreply.github.com>
2020-05-05 20:07:32 -0400

    Edited lots of stuff

--- popup.js
+++ popup.js
@@ -1,37 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
     navigator.geolocation.getCurrentPosition(showMap);
-    // Request to SQL server to change value of lat,lon
-    document.getElementById('send').addEventListener('click',function(){
-    	document.getElementById("result1").innerHTML = ""
-    	var latlon = document.getElementById('loc').innerHTML.split(",")
-    	var xhr = new XMLHttpRequest();
-    	xhr.onreadystatechange = function(){
-    		document.getElementById('result').innerHTML = xhr.responseText;
-    	}
-    	xhr.open("GET","https://contacttracerapp.herokuapp.com/updatelatlon?lat="+latlon[0]+"&lon="+latlon[1],true);
-    	xhr.send();
-    })
-    // Request to SQL server to change value of sick flag
+    // Request to SQL server to change values
     document.getElementById('send1').addEventListener('click',function(){
     	document.getElementById("result").innerHTML = ""
+        document.getElementById("result2").innerHTML = ""
+        document.getElementById("result3").innerHTML = "";
     	var latlon = document.getElementById('loc').innerHTML.split(",")
     	var xhr = new XMLHttpRequest();
     	xhr.onreadystatechange = function(){
     		document.getElementById('result1').innerHTML = xhr.responseText;
     	}
-    	xhr.open("GET","https://contacttracerapp.herokuapp.com/updatesick?sick="+document.getElementById("sick").value,true);
+    	xhr.open("GET","https://contacttracerapp.herokuapp.com/update?sick="+document.getElementById("sick").value+"&lat="+latlon[0]+"&lon="+latlon[1],true);
     	xhr.send();
-    });
+    })
+    // Request for infectedness using the Haversine formula.
     document.getElementById('send2').addEventListener('click',function(){
         document.getElementById("result").innerHTML = "";
         document.getElementById("result1").innerHTML = "";
+        document.getElementById("result3").innerHTML = "";
         var latlon = document.getElementById('loc').innerHTML.split(",");
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function(){
             document.getElementById('result2').innerHTML = xhr.responseText;
         }
-        xhr.open("GET","https://contacttracerapp.herokuapp.com/infected",true);
+        xhr.open("GET","https://contacttracerapp.herokuapp.com/infected?lat="+latlon[0]+"&lon="+latlon[1],true);
         xhr.send();
+    });
+    // Request for history.
+    document.getElementById('hi').addEventListener('click',function(){
+        document.getElementById("result").innerHTML = "";
+        document.getElementById("result1").innerHTML = "";
+        document.getElementById("result2").innerHTML = "";
+        var latlon = document.getElementById('loc').innerHTML.split(",");
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            document.getElementById('result3').innerHTML = xhr.responseText;
+        }
+        xhr.open("GET","https://contacttracerapp.herokuapp.com/history",true);
+        xhr.send();
+    });
+    document.getElementById('so').addEventListener('click',function(){
+        var win = window.open("https://contacttracerapp.herokuapp.com/logout","_blank")
+        win.focus();
     });
 })
 
