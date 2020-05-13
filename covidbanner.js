900d8996b3cdeb709eb021a30e3f88219dfb0739
Jacob-Python <58337545+Jacob-Python@users.noreply.github.com>
2020-05-13 14:23:57 -0400

    Update covidbanner.js

--- covidbanner.js
+++ covidbanner.js
@@ -3,7 +3,7 @@ window.onload = function() {
 	if (uint == 0){
 		// Find instances of "coronavirus" in page
 		let r = 0;
-		var keyPhrases = ["coronavirus", "covid-19","sars-cov-2","2019-ncov"];
+		var keyPhrases = ["coronavirus", "covid-19","sars-cov-2","2019-ncov","coronavirus pandemic", "2019-20 coronavirus pandemic", "coronavirus disease 2019"];
 		for (const key in keyPhrases){
 			if (document.documentElement.innerHTML.toLowerCase().indexOf(key) != -1 || decodeURIComponent(window.location.href.toLowerCase().replace("+"," ")).indexOf(key) != -1){
 				r = 1;
