9879db4334731d66c605ba046e6372a1fc10cd4c
Jacob-Python <58337545+Jacob-Python@users.noreply.github.com>
2020-05-19 10:55:29 -0400

    Updated alots of stuff
    Moved images, added terms and settings, and removed banner

--- popup.js
+++ popup.js
@@ -1,16 +1,5 @@
-let covpopup = true;
 document.addEventListener('DOMContentLoaded', function() { 
     navigator.geolocation.getCurrentPosition(display);
-    var xhr2 = new XMLHttpRequest();
-    xhr2.onreadystatechange = function(){
-       if (xhr2.responseText == '1'){
-            covpopup = true;
-       } else {
-            covpopup = false;
-       }
-    }
-    xhr2.open("GET","https://contacttracerapp.herokuapp.com/popup",true);
-    xhr2.send();
     var xhr1 = new XMLHttpRequest();
     xhr1.onreadystatechange = function(){
        if (xhr1.responseText == '0'){
@@ -21,14 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     xhr1.open("GET","https://contacttracerapp.herokuapp.com/coords",true);
     xhr1.send();
-    // Set slider value to server value.
+    /*Set slider value to server value.*/
     var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function(){
             document.getElementById("sick").value = xhr.responseText;
         }
         xhr.open("GET","https://contacttracerapp.herokuapp.com/issick",true);
         xhr.send();
-    // Request to SQL server to change values
+    /*Request to SQL server to change values*/
     document.getElementById("sett").addEventListener('click',function(){
         window.open("settings.html","_blank")
     })
@@ -45,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
         xhr.open("GET","https://contacttracerapp.herokuapp.com/update?sick="+document.getElementById("sick").value+"&lat="+latlon[0]+"&lon="+latlon[1],true);
         xhr.send();
     })
-    // Request for infectedness using the Haversine formula.
+    /*Request for infectedness using the Haversine formula.*/
     document.getElementById('send2').addEventListener('click',function(){
         document.getElementById("result").innerHTML = "";
         document.getElementById("result1").innerHTML = "";
@@ -59,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
         xhr.open("GET","https://contacttracerapp.herokuapp.com/infected?lat="+latlon[0]+"&lon="+latlon[1],true);
         xhr.send();
     });
-    // Request if you are at risk.
+    /*Request if you are at risk*/
     document.getElementById('send3').addEventListener('click',function(){
         document.getElementById("result").innerHTML = "";
         document.getElementById("result1").innerHTML = "";
@@ -73,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
         xhr.open("GET","https://contacttracerapp.herokuapp.com/atrisk?lat="+latlon[0]+"&lon="+latlon[1],true);
         xhr.send();
     });
-    // Request for history.
+    /*Request for history.*/
     document.getElementById('hi').addEventListener('click',function(){
         document.getElementById("result").innerHTML = "";
         document.getElementById("result1").innerHTML = "";
@@ -89,46 +78,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var win = window.open("https://contacttracerapp.herokuapp.com/logout","_blank")
         win.focus();
     });
-    let tabURL;
-    chrome.browserAction.setPopup({popup: "popup.html"})
-        chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function(tabs) {
-        tabURL = tabs[0].url;
-    });
-    chrome.cookies.get({"name":"covpopup","url":"https://contacttracerapp.herokuapp.com/"},function(cookie){
-        try {
-            var valueJSON = JSON.parse(cookie.value)
-            console.log(valueJSON)
-            if (tabURL == valueJSON.addr && valueJSON.covid == 1){
-                document.getElementById("covidbanner").hidden = false;
-            } else {
-                document.getElementById("covidbanner").hidden = true;
-            }
-        } catch(e) {
-        }
-    })
 })
 function display(data){
     document.getElementById('loc').innerHTML = data.coords.latitude+","+data.coords.longitude;
 }
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    let tabURL;
-    chrome.browserAction.setPopup({popup: "popup.html"})
-        chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function(tabs) {
-        tabURL = tabs[0].url;
-        console.log(tabURL);
-        if (request.covid == 1 && covpopup) {
-            chrome.cookies.set({"url":"https://contacttracerapp.herokuapp.com/","name":"covpopup","value":JSON.stringify({"covid":1,"addr":tabURL})}, function (){})
-            document.getElementById("covidbanner").hidden = false;
-        } else {
-            chrome.cookies.set({"url":"https://contacttracerapp.herokuapp.com/","name":"covpopup","value":JSON.stringify({"covid":0,"addr":tabURL})}, function (){})
-            document.getElementById("covidbanner").hidden = true;
-        }
-    }); 
-});
--- settings.js
+++ settings.js
@@ -1,22 +1,33 @@
 document.addEventListener("DOMContentLoaded", function (){
-	var xhr1 = new XMLHttpRequest();
-    xhr1.onreadystatechange = function(){
-      document.getElementById("alert").value = xhr1.responseText;
+  let loggedin = true;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+    if (xhr.responseText == "True"){
+      loggedin = true;
+      document.getElementById("settings-container").hidden = false;
+      document.getElementById("not-logged-in").hidden = true;
+    } else {
+      loggedin = false;
+      document.getElementById("not-logged-in").hidden = false;
+      document.getElementById("settings-container").hidden = true;
     }
-    xhr1.open("GET","https://contacttracerapp.herokuapp.com/popup",true);
-    xhr1.send();
-    xhr2 = new XMLHttpRequest();
+  }
+  xhr.open("GET","https://contacttracerapp.herokuapp.com/loggedin",true);
+  xhr.send();
+  if (loggedin){
+    var xhr2 = new XMLHttpRequest();
     xhr2.onreadystatechange = function(){
       document.getElementById("display").value = xhr2.responseText;
     }
     xhr2.open("GET","https://contacttracerapp.herokuapp.com/coords",true);
     xhr2.send();
-    document.getElementById("ok").addEventListener("click", function(){
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function(){
-          console.log(xhr.responseText);
-      }
-      xhr.open("GET","https://contacttracerapp.herokuapp.com/updates?popup="+document.getElementById("alert").value+"&coords="+document.getElementById("display").value,true);
-      xhr.send();
-    })
+  }
+  document.getElementById("ok").addEventListener("click", function(){
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        console.log(xhr.responseText);
+    }
+    xhr.open("GET","https://contacttracerapp.herokuapp.com/updates?coords="+document.getElementById("display").value,true);
+    xhr.send();
+  })
 })
